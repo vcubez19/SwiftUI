ad01e5338e1ed69c1f42c3273a6bcd05ecf98736
Vincent Cubit <vinniecubit@yahoo.com>
2022-02-07 13:05:20 -0600

    Initial Commit

---
+++ CovidTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidTrack
+//
+//  Created by Vincent Cubit on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CovidTrack/CovidTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidTrackApp.swift
+//  CovidTrack
+//
+//  Created by Vincent Cubit on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CovidTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
